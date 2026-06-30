77b422b8e054c1b508b9f9729bffcbbd39a89511
eynyte <y.vieutiful@gmail.com>
2026-07-01 07:29:49 +0900

    Update train_ms_jp_extra.py

--- train_ms_jp_extra.py
+++ train_ms_jp_extra.py
@@ -313,7 +313,7 @@ def run():
         train_sampler = DistributedBucketSampler(
             train_dataset,
             hps.train.batch_size,
-            [32, 300, 400, 500, 600, 700, 800, 900, 1000,1500,2000,2500],
+            [32, 300, 400, 500, 600, 700, 800, 900, 1000],
             num_replicas=n_gpus,
             rank=rank,
             shuffle=True,
